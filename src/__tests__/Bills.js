cb9480b64c13d58bb4d6e5045e6baaf736f89f27
aalexandree-g <aalexandree.g@gmail.com>
2025-07-24 16:23:26 +0200

    Tests containers/Bills done

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+import "@testing-library/jest-dom"
 import {screen, waitFor, fireEvent} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import Bills from "../containers/Bills.js"
@@ -19,24 +20,16 @@ describe("Given I am connected as an employee", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
       // localStorage simulation
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-
-      // connected employee similation
+      // connected employee simulation
       window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
-
       // DOM initialization
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
       router()
-
       // environment simulation
       window.onNavigate(ROUTES_PATH.Bills)
-      billsContainer = new Bills({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage
-      })
+      billsContainer = new Bills({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
 
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
@@ -58,24 +51,16 @@ describe("Given I am connected as an employee", () => {
       test("Then it should open the new bill form", () => {
         // localStorage simulation
         Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-
-        // connected employee similation
+        // connected employee simulation
         window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
-
         // DOM initialization
         const root = document.createElement("div")
         root.setAttribute("id", "root")
         document.body.append(root)
         router()
-
         // environment simulation
         window.onNavigate(ROUTES_PATH.Bills)
-        billsContainer = new Bills({
-          document,
-          onNavigate,
-          store: mockStore,
-          localStorage: window.localStorage
-        })
+        billsContainer = new Bills({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
 
         const newBillBtn = screen.getByTestId("btn-new-bill")
         // spy handleClickNewBill function
@@ -90,51 +75,110 @@ describe("Given I am connected as an employee", () => {
       })
     })
 
-    describe("When I click on the eye icon", () => {
+    describe("When I click on the eye icon to preview a bill", () => {
       test("Then it should open a modal", () => {
         // localStorage simulation
         Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-
-        // connected employee similation
+        // connected employee simulation
         window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
-
         // DOM initialization
         const root = document.createElement("div")
         root.setAttribute("id", "root")
         document.body.append(root)
         router()
-
         // environment simulation
         window.onNavigate(ROUTES_PATH.Bills)
-        billsContainer = new Bills({
-          document,
-          onNavigate,
-          store: mockStore,
-          localStorage: window.localStorage
-        })
+        billsContainer = new Bills({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
 
-        /*$.fn.modal = jest.fn() // mock bootstrap modal
-
+        // modal simulation
+        $.fn.modal = jest.fn()
+        
         document.body.innerHTML = BillsUI({ data: bills })
-        const iconEye = screen.getAllByTestId("icon-eye")[0]
-        const billUrl = iconEye.getAttribute("data-bill-url")
 
-        const handleClickIconEye = jest.fn(() =>
-          billsContainer.handleClickIconEye(iconEye)
-        )
-        iconEye.addEventListener("click", () => handleClickIconEye(iconEye))
+        // get the first eye icon
+        const modal = document.getElementById("modaleFile")
+        const iconEye = screen.getAllByTestId("icon-eye")[0]
+
+        // spy handleClickIconEye function
+        const handleClickIconEye = jest.fn(() => billsContainer.handleClickIconEye(iconEye))
+        // attach function to click
+        iconEye.addEventListener("click", handleClickIconEye)
+        // click simulation
         fireEvent.click(iconEye)
 
         expect(handleClickIconEye).toHaveBeenCalled()
-        expect($.fn.modal).toHaveBeenCalled()
-        expect(screen.getByRole("img").getAttribute("src")).toBe(billUrl)*/
+        expect(modal).toBeTruthy()
+      })
+    })
+
+    describe("and it is loading", () => {
+      test("Then loading page should be rendered", () => {
+        document.body.innerHTML = BillsUI({ loading: true })
+        expect(screen.getByText(/loading\.\.\./i)).toBeInTheDocument()
+      })
+    })
+
+    describe("and it throws an error", () => {
+      test("Then error page should be rendered", () => {
+        document.body.innerHTML = BillsUI({ error: "An error occurs" })
+        expect(screen.getByText(/error/i)).toBeInTheDocument()
       })
     })
   })
 
-  
+  // integration test GET
+  describe("Given I am a user connected as Admin", () => {
+    describe("When I navigate to Dashboard", () => {
+      let billsContainer
 
+      test("Then it should fetch the list of bills from the mock API", () => {
+        // localStorage simulation
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        // connected admin simulation
+        localStorage.setItem("user", JSON.stringify({ type: "Admin", email: "a@a" }))
+        // DOM initialization
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        // environment simulation
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        billsContainer = new Bills({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
 
+        billsContainer.getBills().then(data => {
+          root.innerHTML = BillsUI({ data })
+          expect(document.querySelector('tbody').rows.length).toBeGreaterThan(0)
+        })
+      })
+    })
 
-  
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        // localStorage simulation
+        Object.defineProperty( window, 'localStorage', { value: localStorageMock })
+        // connected admin simulation
+        window.localStorage.setItem('user', JSON.stringify({ type: 'Admin', email: "a@a" }))
+        // DOM initialization
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        router()
+        // environment simulation
+        window.onNavigate(ROUTES_PATH.Dashboard)
+      })
+
+      test("Then it should fail with 404 message error", () => {
+        document.body.innerHTML = BillsUI({ error: 'Erreur 404' })
+        const message = screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+
+      test("Then it should fail with 500 message error", () => {
+        document.body.innerHTML = BillsUI({ error: 'Erreur 500' })
+        const message = screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+  })
 })
